4933d97e278ff7fde29776bb2d9edcc8f517e654
Nathan Beltran nbelt86@gmail.com <nbelt86@gmail.com>
2026-08-05 17:51:36 +0900

    changed a lot on the show page now. not quite done with it though.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,6 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
-export default class extends Controller {
-  static targets =  ["content"]
+import { Controller } from "@hotwired/stimulus" //  @hotwired/stimulus is a package
+//  (a library of pre-written code) that includes a class called Controller,
+// this import reaches into the package and pulls a reference to that specific
+// Controller class into your file, under the name Controller so that we can
+// use it the following line...
+export default class extends Controller { //creates a brand new class that inherits
+  // that Controller knows how to do plus the two things we add below
+  static targets =  ["content"] // be able to grab an element and call it "content"
   toggleVisibility() {
     this.contentTarget.classList.toggle("hidden")
   }
