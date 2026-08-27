5727394bafce6cbe4777920634bb8fc5df970404
Nathan Beltran nbelt86@gmail.com <nbelt86@gmail.com>
2026-08-27 11:16:39 +0900

    Validate that workout date cannot be in the future

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,13 @@ class Workout < ApplicationRecord
   has_many :exercise_entries, dependent: :destroy
 
   validates :name, presence: true
+  validate :date_cannot_be_in_the_future
+
+  private
+
+  def date_cannot_be_in_the_future
+    return if date.blank?
+
+    errors.add(:date, "can't be in the future") if date > Date.today
+  end
 end
