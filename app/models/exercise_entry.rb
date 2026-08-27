e37679d0661fcc53097d3aad8bd9d98d9d51d2a7
Nathan Beltran nbelt86@gmail.com <nbelt86@gmail.com>
2026-08-27 11:19:00 +0900

    Show reps/weight from the previous time an exercise was performed, for progression tracking

--- app/models/exercise_entry.rb
+++ app/models/exercise_entry.rb
@@ -2,4 +2,13 @@ class ExerciseEntry < ApplicationRecord
   belongs_to :exercise
   belongs_to :workout
   has_many :set_entries, dependent: :destroy
+
+  def previous_entry
+    ExerciseEntry.joins(:workout)
+                 .where(exercise_id: exercise_id)
+                 .where.not(id: id)
+                 .where.not(workout_id: workout_id)
+                 .order("workouts.date DESC, workouts.id DESC")
+                 .first
+  end
 end
